3570ae436a300302f15c1eff10d2a45b97f1e8a6
Rodri <r.serranvives@gmail.com>
2025-09-25 18:15:29 +0200

    feat: enhance room management with marked and called numbers functionality

--- src/store/roomStore.js
+++ src/store/roomStore.js
@@ -15,10 +15,29 @@ class InMemoryRoomStore extends RoomStore {
         this.rooms = new Map();
     }
     findRoom(id) {
+        // TODO: add redis layer
         return this.rooms.get(id)
     }
-    saveRoom(id, { roomId, code, admin, players = new Map() }) {
-        this.rooms.set(id, { roomId, code, admin, players })
+    saveRoom(id, { roomId, code, admin, players = new Map(), calledNumbers = new Set() }) {
+        // TODO: add redis layer
+        //redis
+        // const savedRedisData = await JSON.parse(await redisClient.get('roomCodes'))
+        // const newValue = {}
+        // newValue[roomId] = code
+        // const newRedisData = { ...savedRedisData, ...newValue }
+        // const updatedRedisData = await redisClient.set('roomCodes', JSON.stringify(newRedisData))
+        // console.log('saved redis data', updatedRedisData, roomId)
+
+        this.rooms.set(id, { roomId, code, admin, players, calledNumbers })
+    }
+    checkCode(roomId, code) {
+
+        // TODO: add redis layer
+        // const redisRoomCodes = await JSON.parse(await redisClient.get('roomCodes'))
+        // const realCode = redisRoomCodes[roomId]
+        // const room = roomStore.findRoom(roomId)
+
+        return code === this.findRoom(roomId)?.code
     }
     addPlayer(id, { playerID, player }) {
         const room = this.findRoom(id)
@@ -26,6 +45,28 @@ class InMemoryRoomStore extends RoomStore {
             const updatedPlayers = room.players.set(playerID, player)
             this.saveRoom(id, { ...room, players: updatedPlayers })
         }
+    }
+    findMarkedNumbers(roomId, playerId) {
+        console.log('find marked', roomId)
+        const room = this.findRoom(roomId)
+        const player = room?.players.get(playerId)
+
+        if (!player) {
+            return null
+        }
+        if (player.markedNumbers === null || player.markedNumbers === undefined) {
+            player.markedNumbers = new Set()
+            room.players.set(playerId, { ...player })
+            this.saveRoom(roomId, { ...room })
+        }
+        return player.markedNumbers
+
+    }
+    addMarkedNumber(roomId, playerId, number) {
+        console.log('add marked', roomId, playerId, number)
+        const markedNumbers = this.findMarkedNumbers(roomId, playerId)
+        markedNumbers.add(number)
+
     }
     removePlayer(id, { playerID }) {
         const room = this.findRoom(id)
@@ -35,9 +76,24 @@ class InMemoryRoomStore extends RoomStore {
             this.saveRoom(id, { ...room, players: updatedPlayers })
         }
     }
+    addCalledNumber(roomId, number) {
+        const calledNumbers = this.findCalledNumbers(roomId)
+        console.log('before adding', calledNumbers, number)
+        calledNumbers.add(number)
+        console.log('added', calledNumbers)
+        return calledNumbers
+    }
+    findCalledNumbers(roomId) {
+        const room = this.findRoom(roomId)
+        // console.log('foundcalled numbers', room?.calledNumbers)
+
+        return room?.calledNumbers ?? new Set()
+    }
+
     roomAdmin(roomId) {
         const room = this.rooms.get(roomId)
-        return room.admin
+
+        return room ? room.admin : null
     }
     removeRoom(id) {
         this.rooms.delete(id)

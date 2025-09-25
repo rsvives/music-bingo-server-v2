56bf8041d7b7a6a40437cdf35d9d3f3ab5ddc38a
Rodri <r.serranvives@gmail.com>
2025-09-25 18:13:54 +0200

    feat: enhance bingo number generation to support multiple players

--- src/lib/bingoNumbers.js
+++ src/lib/bingoNumbers.js
@@ -6,41 +6,45 @@ export const generateInitialNumbers = (maxNumero = 90) => {
 }
 
 
-export const generateBingoNumbers = (cantidadCartones = 6, numerosPorCarton = 15, maxNumero = 90) => {
-    const { initialNumbersSet } = generateInitialNumbers()
+export const generateBingoNumbers = ({ cantidadCartones = 6, numerosPorCarton = 15, maxNumero = 90, jugadores = 1 }) => {
     const cards = []
+    console.log(jugadores)
 
-    for (let i = 1; i <= cantidadCartones; i++) {
-        const [cardboard] = Array.from({ length: 1 }, (v, k) => {
-            const set = new Set()
-            while (set.size < 15) {
-                const randomNumberIndex = Math.floor(Math.random() * initialNumbersSet.size)
-                // console.log(randomNumberIndex)
-                const numbersArray = Array.from(initialNumbersSet)
-                const chosenValue = numbersArray[randomNumberIndex]
-                set.add(chosenValue)
-                initialNumbersSet.delete(chosenValue)
-            }
-            return Array.from(set)
-        })
-        //console.log(cardboard)
-        const orderedCardboard = cardboard.sort((a, b) => a - b)
-        //console.log(i,orderedCardboard)
-        const columnsArray = [
-            orderedCardboard.slice(0, 3),
-            orderedCardboard.slice(3, 6),
-            orderedCardboard.slice(6, 9),
-            orderedCardboard.slice(9, 12),
-            orderedCardboard.slice(12, 15),
-        ]
+    for (let v = 1; v <= Math.ceil(jugadores / cantidadCartones); v++) {
+        const { initialNumbersSet } = generateInitialNumbers()
 
-        // Transpose columns to rows
-        const transposedCard = columnsArray[0].map((_, colIndex) =>
-            columnsArray.map(row => row[colIndex])
-        )
+        for (let i = 1; i <= cantidadCartones; i++) {
+            const [cardboard] = Array.from({ length: 1 }, (v, k) => {
+                const set = new Set()
+                while (set.size < 15) {
+                    const randomNumberIndex = Math.floor(Math.random() * initialNumbersSet.size)
+                    // console.log(randomNumberIndex)
+                    const numbersArray = Array.from(initialNumbersSet)
+                    const chosenValue = numbersArray[randomNumberIndex]
+                    set.add(chosenValue)
+                    initialNumbersSet.delete(chosenValue)
+                }
+                return Array.from(set)
+            })
+            //console.log(cardboard)
+            const orderedCardboard = cardboard.sort((a, b) => a - b)
+            //console.log(i,orderedCardboard)
+            const columnsArray = [
+                orderedCardboard.slice(0, 3),
+                orderedCardboard.slice(3, 6),
+                orderedCardboard.slice(6, 9),
+                orderedCardboard.slice(9, 12),
+                orderedCardboard.slice(12, 15),
+            ]
 
-        console.log('transposed', transposedCard)
-        cards.push(transposedCard)
+            // Transpose columns to rows
+            const transposedCard = columnsArray[0].map((_, colIndex) =>
+                columnsArray.map(row => row[colIndex])
+            )
+
+            console.log('transposed', transposedCard)
+            cards.push(transposedCard)
+        }
     }
 
     console.log('generating bingo', cards)
